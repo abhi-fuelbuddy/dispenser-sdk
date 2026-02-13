3829bab9994de383266b74c813d6eee5d425a789
abhi-fuelbuddy <abhishek.ramola@fuelbuddy.in>
2026-02-13 17:58:45 +0530

    refactor: read sale choose processor based on dispenser type

--- .bin/read-sale.ts
+++ .bin/read-sale.ts
@@ -7,8 +7,21 @@ import { getConfigFromEnv } from '../utils/envParser';
 const debugLog = debug('dispenser:main');
 const configuration = getConfigFromEnv();
 debugLog('Configuration: %O', configuration);
+
 createDispenser(configuration).then((dispenser) => {
-	dispenser.execute(dispenser.readSale, dispenser.isOrderComplete).then((response) => {
+	// Neogi uses LT command which returns full transaction details
+	// Other dispensers may return order completion status
+	const isNeogi = dispenser.checkType?.() === 'NEOGI';
+	const processor = isNeogi
+		? dispenser.processReadSale
+		: dispenser.isOrderComplete;
+
+	debugLog('Dispenser type: %s, Using processor: %s',
+		dispenser.checkType?.() || 'Unknown',
+		isNeogi ? 'processReadSale' : 'isOrderComplete'
+	);
+
+	dispenser.execute(dispenser.readSale, processor).then((response) => {
 		dispenser.disconnect(() => {
 			console.log('Disconnected');
 		});
